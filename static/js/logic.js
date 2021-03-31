e8d27286f201a2c115af2aa456f6024ad41b7707
Amir Vazquez <j.amir.vazquez@gmai.com>
2021-03-30 21:43:19 -0700

    testing code Map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,8 @@
+/*
+Initialize Global Variables
+*/
+var url_usgs = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+
 function createMap(quakes) {
 
     // Create the tile layer that will be the background of our map
@@ -19,7 +24,7 @@ function createMap(quakes) {
     };
   
     // Create the map object with options Los Angeles, CA
-    var map = L.map("map-id", {
+    var Map = L.map("mapid", {
       center: [34.0522, 118.2437],
       zoom: 12,
       layers: [lightmap, quakes]
@@ -28,7 +33,47 @@ function createMap(quakes) {
     // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
     L.control.layers(baseMaps, overlayMaps, {
       collapsed: false
-    }).addTo(map);
+    }).addTo(Map);
+  }
+
+function createQuakeSpots(response) {
+      console.log(response);
+
+    // Pull Quate Spots from Features of our data pull 
+    var qSpots = response.features;
+  
+    // Loop through the quakes array
+    for (var index = 0; index < qSpots.length; index++) {
+      var qSpot = qSpots[index].geometry.coordinates;
+      //console.log(qSpot);
+
+      var qMagnitude = qSpots[index].properties.mag;
+      var qPlace = qSpots[index].properties.place;
+      var qId = qSpots[index].properties.ids;
+  
+      L.circle(qSpot, {
+          color:"white",
+          fillcolor: "#FF0000",
+          radius: qMagnitude * 1000
+
+      }).bindPopup(`<center><h1> ${qMagnitude} Quake Magnitud</h1></center>
+      <ul>
+        <li>Place: ${qPlace}</li>
+        <li>Coordinates: ${qSpot}</li>
+        <li>ID: ${qId}</li>
+      </ul>`
+
+      ).addTo(Map);
+  
+    }
+  
+    createMap(L.layerGroup(qSpots));
   }
 
   
+
+
+
+  //d3.json(url_usgs, function (response){     console.log(response);  })
+
+  d3.json(url_usgs,createQuakeSpots);
