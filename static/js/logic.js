c35295a98380a2da75a02a34cf7b1131b066c64c
Amir Vazquez <j.amir.vazquez@gmai.com>
2021-03-31 00:44:29 -0700

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,44 +1,26 @@
 /*
 Initialize Global Variables
 */
+// URL
 var url_usgs = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
-    // Create the map object with options Los Angeles, CA
-var Map = L.map("mapid", {
-      center: [34.0522, 118.2437],
-      zoom: 12
-      //,layers: [lightmap, quakes]
+// Create lakers for Map and Quakes
+var quakesLayer = L.layerGroup();
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "light-v10",
+  accessToken: API_KEY
 });
-  
-function createMap(quakes) {
 
-    // Create the tile layer that will be the background of our map
-    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "light-v10",
-      accessToken: API_KEY
-    });
-  
-    // Create a baseMaps object to hold the lightmap layer
-    var baseMaps = {
-      "Light Map": lightmap
-    };
-  
-    // Create an overlayMaps object to hold the quakes layer
-    var overlayMaps = {
-      "Quates": quakes
-    };
-  
-
-    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(Map);
-  }
-
+// Create the map object with options Los Angeles, CA
+var Map = L.map("mapid", {
+  center: [34.0522, 118.2437],
+  zoom: 5,
+  layers: [lightmap, quakesLayer]
+});
 
 function createQuakeSpots(response) {
-      console.log(response);
+     // console.log(response);
 
     // Pull Quate Spots from Features of our data pull 
     var qSpots = response.features;
@@ -65,17 +47,13 @@ function createQuakeSpots(response) {
         <li>ID: ${qId}</li>
       </ul>`
 
-      ).addTo(Map);
-  
+      ).addTo(quakesLayer);
+      quakesLayer.addTo(Map);
     }
   
-    createMap(L.layerGroup(QuakeSpots));
+//    createMap(L.layerGroup(QuakeSpots));
     console.log(QuakeSpots);
   }
-
-  
-
-
 
   //d3.json(url_usgs, function (response){     console.log(response);  })
 
